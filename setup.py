d04bb09430b6ea0240a2152c32a5b99d29aad353
TimeTraveller-San <anonymous.yolo.wtf@gmail.com>
2018-12-11 12:15:34 +0000

    minor changes v1.1.2

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 import setuptools
 
-reqs = (line.strip() for line in open("requirements.txt"))
+# reqs = (line.strip() for line in open("requirements.txt"))
 LONG_DESC = open('README.md').read()
 setuptools.setup(
     name="yTermPlayer",
-    version="1.0.0",
+    version="1.1.2",
     author="Time Traveller",
-    author_email="notSharing@ever.com",
+    author_email="time.traveller.san@gmail.com",
     description="Play youtube playlists as audio on linux terminal ",
     long_description=LONG_DESC,
     long_description_content_type="text/markdown",
     url="https://github.com/TimeTraveller-San/yTermPlayer",
     packages=setuptools.find_packages(),
     license="GPLv3",
-    install_requires=reqs,
+    install_requires=[
+                'pafy>=0.5.4',
+                'python-dateutil>=2.7.3',
+                'python-mpv>=0.3.9',
+                'urwid>=2.0.1',
+                'virtualenv>=15.1.0',
+                'youtube-dl>=2018.8.4',
+    ],
     python_requires='>=3',
     classifiers=[
         "Programming Language :: Python :: 3",
